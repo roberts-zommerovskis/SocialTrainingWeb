dcd8752546066c61d4498afba8f91fff327c8f87
Roberts Zommerovskis <roberts.zommerovskis@visma.com>
2017-07-28 10:29:32 +0300

    Revert "Test"
    This reverts commit 994724fba6826064c56c69206b81a18e187b92f5.

--- SocialTrainingWebApp/Controllers/AccountController.cs
+++ SocialTrainingWebApp/Controllers/AccountController.cs
@@ -12,7 +12,6 @@ using SocialTrainingWebApp.Models;
 
 namespace SocialTrainingWebApp.Controllers
 {
-    //
     [Authorize]
     public class AccountController : Controller
     {
@@ -23,7 +22,7 @@ namespace SocialTrainingWebApp.Controllers
         {
         }
 
-        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
         {
             UserManager = userManager;
             SignInManager = signInManager;
@@ -35,9 +34,9 @@ namespace SocialTrainingWebApp.Controllers
             {
                 return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
             }
-            private set
-            {
-                _signInManager = value;
+            private set 
+            { 
+                _signInManager = value; 
             }
         }
 
@@ -121,7 +120,7 @@ namespace SocialTrainingWebApp.Controllers
             // If a user enters incorrect codes for a specified amount of time then the user account 
             // will be locked out for a specified amount of time. 
             // You can configure the account lockout settings in IdentityConfig
-            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent: model.RememberMe, rememberBrowser: model.RememberBrowser);
+            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent:  model.RememberMe, rememberBrowser: model.RememberBrowser);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -156,8 +155,8 @@ namespace SocialTrainingWebApp.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
-
+                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
+                    
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
