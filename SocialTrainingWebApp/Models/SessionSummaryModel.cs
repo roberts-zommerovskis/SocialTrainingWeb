a9f7e1d79cbd1e6caec39b5e9d579553a149128f
Roberts Zommerovskis <robbzomm@gmail.com>
2017-08-30 09:56:29 +0300

    Frontend - removal of unused functionality, update to model usage instead of hard-coded strings

--- SocialTrainingWebApp/Models/SessionSummaryModel.cs
+++ SocialTrainingWebApp/Models/SessionSummaryModel.cs
@@ -11,6 +11,7 @@ namespace SocialTrainingWebApp.Models
         public double _percentageValue;
         public string _percentageString;
         public string _congratulationsButtonText;
+        public string _informativeGameText;
         public SessionSummaryModel(int originalEmployeeCountInDb, double guessedEmployees, bool gameCompleted)
         {
             _percentageValue = Math.Round(((guessedEmployees / originalEmployeeCountInDb) * 100), 0, MidpointRounding.ToEven);
@@ -18,10 +19,12 @@ namespace SocialTrainingWebApp.Models
             if (gameCompleted)
             {
                 _congratulationsButtonText = "Play again!";
+                _informativeGameText = "Game over!";
             }
             else
             {
                 _congratulationsButtonText = "Continue game!";
+                _informativeGameText = "Well done!";
             }
         }
     }
