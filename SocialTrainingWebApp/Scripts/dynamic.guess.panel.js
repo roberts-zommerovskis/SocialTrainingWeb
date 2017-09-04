8a3a79ce9d1c52a3cc896401670e98913f407823
Roberts Zommerovskis <robbzomm@gmail.com>
2017-09-04 18:28:25 +0300

    Frontend: minor tweaks

--- SocialTrainingWebApp/Scripts/dynamic.guess.panel.js
+++ SocialTrainingWebApp/Scripts/dynamic.guess.panel.js
@@ -7,13 +7,13 @@ $(document).ready(adjustPercentage);
 function resizePanel() {
     var windowWidth = $(window).width();
     var windowHeight = $(window).height();
-    if ((windowWidth < 1450 && windowWidth > 630) && $(".container-fluid").width != 640) {
+    if (windowWidth < 1450 && windowWidth > 630 && $(".container-fluid").width !== 640) {
         $(".container-fluid").css("width", "640px");
     }
-    else if (windowWidth < 630 && $(".container-fluid").width != "50%") {
+    else if (windowWidth < 630 && $(".container-fluid").width !== "50%") {
         $(".container-fluid").css("width", "90%");
     }
-    if (windowWidth < 775 && $(".container-fluid").width != "80%") {
+    if (windowWidth < 775 && $(".container-fluid").width !== "80%") {
         $(".container-fluid").css("height", "80%");
     }
     else {
